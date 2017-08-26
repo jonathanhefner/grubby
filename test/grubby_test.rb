a17cde719c746e426f32959339761a25d32f35a0
Jonathan Hefner <jonathan.hefner@gmail.com>
2017-08-26 14:43:50 -0500

    Remove default test

--- test/grubby_test.rb
+++ test/grubby_test.rb
@@ -1,11 +1,9 @@
 require "test_helper"
 
 class GrubbyTest < Minitest::Test
+
   def test_that_it_has_a_version_number
     refute_nil ::Grubby::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
-  end
 end
